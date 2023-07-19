2e33ba55e54d0a8380bdd437219da67f3be224bc
kunikhin <evgenii.kunikhin@ya.ru>
2023-07-19 11:54:50 +0300

    service lay annotation fix

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,13 +1,13 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class CarServiceImp implements CarService{
     private List<Car> cars;
     private static int ID_INCR = 0;
